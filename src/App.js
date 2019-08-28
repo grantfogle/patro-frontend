dc485a2254a435d6f4f6cda450a433d6b3f0fb4e
grantfogle <grantjfogle@gmail.com>
2019-08-27 21:56:11 -0600

    connected dashboard to front end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Dashboard />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import '../App.css';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <h1>Dashboard</h1>
+        );
+    }
+}
+
+export default Dashboard;
