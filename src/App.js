0a07b59387bdaa878c85398528d77b58f823922f
grantfogle <grantjfogle@gmail.com>
2019-09-05 19:25:19 -0600

    making updates to dashboard

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Navigation from './components/Navigation';
 import Goals from './components/Goals';
-import './App.css';
+
 
 function App() {
   return (
     <Router>
-      <div className="App">
-          <Navigation />
-          <div>
-            <Route exact path="/" component={() => <Dashboard />}/>
-            <Route exact path="/goals" component={() => <Goals />}/>
-          </div>
-          {/* <Dashboard />
-          <Goals /> */}
+
+      <Navigation />
+      <div>
+        <Route exact path="/" component={() => <Dashboard />} />
+        <Route exact path="/goals" component={() => <Goals />} />
       </div>
+      {/* <Dashboard />
+          <Goals /> */}
+
     </Router>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import '../App.css';
+// import '../App.css';
 
 class Dashboard extends Component {
     render() {
         return (
-            <div className="Dashboard">
-                <h1>Dashboard</h1>
+            <div className="dashboard">
+                <h1>Your Current Goals</h1>
+                <h4>September 5th, 2019</h4>
+                <p>You have _____ of goals. And are 40% to your goal of ...</p>
+                <h5>Your current goals:</h5>
+                <ul>
+                    <li>Lift weights till you get strong</li>
+                    <li>Move out of the country</li>
+                    <li>Lift weights till you get strong</li>
+                </ul>
             </div>
-            
+
         );
     }
 }
