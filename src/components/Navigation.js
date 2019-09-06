11633c61aef19efead462fc1a749cf2b51f88880
grantfogle <grantjfogle@gmail.com>
2019-09-05 18:51:16 -0600

    added styling to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <div className="navigation">
+        <div className='navigation'>
             <p>Patro Logo</p>
             <ul>
-                <Link to="/goals">
+                <Link to='/goals' style={{ textDecoration: 'none' }}>
                     <li>View Goals</li>
                 </Link>
-                <Link to="/">
+                <Link to='/' style={{ textDecoration: 'none' }}>
                     <li>Progress</li>
-                </Link>                
+                </Link>
                 <li>Add Goal</li>
             </ul>
             {/* User  */}
