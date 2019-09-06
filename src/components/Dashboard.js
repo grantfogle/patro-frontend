72cbe324687cfbab1ab306ef11b82df9b0c8973c
Grant <grant.fogle@davita.com>
2019-09-06 14:35:12 -0600

    adding background image, might remove, shutting down work for the weekend

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,15 +8,15 @@ class Dashboard extends Component {
         return (
             <>
                 <div className="dashboard">
-                    <h1>Your Current Goals</h1>
+                    <h1>Hey, Grant</h1>
                     <h4>September 5th, 2019</h4>
                     <p>You have _____ of goals. And are 40% to your goal of ...</p>
-                    <h5>Your current goals:</h5>
+                    {/* <h5>Your current goals:</h5>
                     <ul>
                         <li>Lift weights till you get strong</li>
                         <li>Move out of the country</li>
                         <li>Lift weights till you get strong</li>
-                    </ul>
+                    </ul> */}
                 </div>
                 <Goals></Goals>
             </>
