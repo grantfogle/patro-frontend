32687b6819075ac57747d32c03fc05ac5a8027e5
Grant <grant.fogle@davita.com>
2019-09-12 15:50:45 -0600

    added goal form to app

--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -14,26 +14,35 @@ class GoalForm extends Component {
         return (
             <div className="goal-form">
                 <h1>Goal Setting</h1>
-                <p></p>
+                <p>Goal setting is the process of designing your goals in a way in which you can better achieve them and measure your progress in them.</p>
+                <p>This specific guid deals with setting SMART Goals, Smart goals are:</p>
+                <ul>
+                    <li><b>Specific.</b> What will be accomplished? What actions will you take?</li>
+                    <li><b>Measureable.</b> What data will measure the goal? (How much? How well?)</li>
+                    <li><b>Attainable.</b> Is the goal doable? Do you have the necessary skills and resources?</li>
+                    <li><b>Relevant.</b> How does the goal align with broader goals? Why is the result important?</li>
+                    <li><b>Time Based.</b> What is the time frame for accomplishing the goal?</li>
+                </ul>
+                <p>For more information on goal setting practices, <a href="#">James Clear offers a great guide</a></p>
                 <form>
                     <div className="form-group">
-                        <label for="formGroupExampleInput"><h3>What is the goal you are trying to achieve?</h3></label>
-                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input" />
+                        <label for="formGroupExampleInput"><h4>What is the goal you are trying to achieve?</h4></label>
+                        <textarea type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input" />
                     </div>
 
                     <div className="form-group">
-                        <label for="formGroupExampleInput2"><h3>Is your goal attainable within your time constraints?</h3></label>
-                        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
+                        <label for="formGroupExampleInput2"><h4>Is your goal attainable within your time constraints?</h4></label>
+                        <textarea type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
                     </div>
                     <div className="form-group">
-                        <label for="formGroupExampleInput2"><h3>What specific behaviors/habits do you plan on changing to achieve your goal</h3></label>
-                        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
+                        <label for="formGroupExampleInput2"><h4>What specific behaviors/habits do you plan on changing to achieve your goal</h4></label>
+                        <textarea type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
                     </div>
                     <div className="form-group">
-                        <label for="formGroupExampleInput2"><h3>When do you hope to achieve this goal by?</h3></label>
+                        <label for="formGroupExampleInput2"><h4>When do you hope to achieve this goal by?</h4></label>
                         <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
                     </div>
-                    <button type="submit">Create Goal</button>
+                    <button type="submit" className="btn btn-primary">Create Goal</button>
                 </form>
             </div>
         
