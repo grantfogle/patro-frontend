53980831a9815f8c18dbc07a46c680827b31d03b
grantfogle <grantjfogle@gmail.com>
2019-09-13 17:42:44 -0600

    are my commits registering on git

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,18 +3,19 @@ import Goals from './Goals';
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import '../App.css';
 
-class Dashboard extends Component {    
+class Dashboard extends Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = { date: new Date() };
     }
-    
+
     render() {
         return (
             <div className="dashboard">
                 <div class="dashboard-quote">
                     <h1 className="text-center"><i className="fa fa-quote-left"></i><i>The happiness of your life depends upon the quality of your thoughts.</i> <i className="fa fa-quote-right"></i></h1>
                     <h4 className="text-right">Marcus Aureileus</h4>
+
                 </div>
                 <Goals></Goals>
             </div>
