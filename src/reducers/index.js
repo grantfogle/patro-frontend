78270874fbc1773703a2a9a216ad546e1d3acd3f
Grant <grant.fogle@davita.com>
2019-09-17 16:35:55 -0600

    cleaning up add goal action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 
 // initialState = stuff: []
 
-let mockData = [{id: 1, name: "Get that bread", timeFrame: "January 2020", priority: "high", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", displayGoal: false},
-        {id: 2, name: "Meet a nice lady", timeFrame: "January 2021", priority: "medium", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", displayGoal: false},
-        {id: 3, name: "Get swole", timeFrame: "June 2022", priority: "low", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", displayGoal: false},
-        {id: 4, name: "Travel More", timeFrame: "May 2020", priority: "high", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", displayGoal: false},
-        {id: 5, name: "Purple Belt BJJ", timeFrame: "May 2020", priority: "high", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", displayGoal: false}];
+let mockData = [{id: 1, name: "Get that bread", timeFrame: "January 2020", priority: "high", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", attainability: 'Yea dude i got this', habits:'floss'},
+        {id: 2, name: "Meet a nice lady", timeFrame: "January 2021", priority: "medium", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", attainability: 'Yea dude i got this', habits:'floss'},
+        {id: 3, name: "Get swole", timeFrame: "June 2022", priority: "low", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", attainability: 'Yea dude i got this', habits:'floss'},
+        {id: 4, name: "Travel More", timeFrame: "May 2020", priority: "high", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", attainability: 'Yea dude i got this', habits:'floss'},
+        {id: 5, name: "Purple Belt BJJ", timeFrame: "May 2020", priority: "high", description: "asdfsdf asd asdf asdf asdf asdfasdfasdf", attainability: 'Yea dude i got this', habits:'floss'}];
 
 function goals(state = [mockData], action) {
     switch (action.type) {
